19d84aa2d6fba208b65145b10d242d1d216e388c
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-05-05 20:51:08 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,7 @@ const Listing = require("../models/listing");
 
 module.exports.index = async(req, res) => {
   const allListings = await Listing.find({});
-  res.render("listing/index", { allListings });
+  res.render("listing/index", {allListings});
 };
 
 module.exports.renderNewForm = (req,res) => {
